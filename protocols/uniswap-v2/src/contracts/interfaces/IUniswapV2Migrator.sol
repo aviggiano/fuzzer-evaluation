dba8859598f71f1e10c19e93d2d2123166bf3355
Antonio Guilherme Ferreira Viggiano <agfviggiano@gmail.com>
2023-06-27 17:02:14 -0300

    chore: forge init

---
+++ protocols/uniswap-v2/src/contracts/interfaces/IUniswapV2Migrator.sol
@@ -0,0 +1,11 @@
+pragma solidity ^0.8.0;
+
+interface IUniswapV2Migrator {
+    function migrate(
+        address token,
+        uint amountTokenMin,
+        uint amountETHMin,
+        address to,
+        uint deadline
+    ) external;
+}
