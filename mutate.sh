2790a3ac9f4189fcc30885ca471d2e0e99fc2874
Antonio Guilherme Ferreira Viggiano <agfviggiano@gmail.com>
2023-06-30 00:17:37 -0300

    Fix bash script

--- mutate.sh
+++ mutate.sh
@@ -8,8 +8,8 @@ for PROTOCOL in $(ls protocols); do
 
 	mkdir -p mutants/slither
 	i=0
-	for FILE in $(find src -type f -name '*.sol' | grep -v interface); do 
-		let i++
+	for FILE in $(find src -type f -name '*.sol'); do 
+		i=$((i+1))
 		slither-mutate $FILE --solc-remaps @openzeppelin/=lib/openzeppelin-contracts/contracts --solc-remaps @uniswap/=src/ > mutants/slither/$(printf "%02d" $i).patch
 	done
 	cd -
