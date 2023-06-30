d7bc31be20d0d32442d432ff64066e8ab431a584
Antonio Guilherme Ferreira Viggiano <agfviggiano@gmail.com>
2023-06-30 16:22:52 -0300

    Update order: protocol,mutant,seed

--- evaluate.sh
+++ evaluate.sh
@@ -10,36 +10,34 @@ echo "slither=$(slither --version)" >> $PARAMETERS
 echo "forge=$(forge --version)" >> $PARAMETERS
 echo "solc=$(solc --version | head -2 | tail -1)" >> $PARAMETERS
 
-echo "fuzzer,protocol,seed,mutant,time,result" > $RESULTS
+echo "fuzzer,protocol,mutant,seed,time,result" > $RESULTS
 
-for SEED in $(cat seeds.txt); do
-	for PROTOCOL in $(ls protocols); do
-		cd protocols/$PROTOCOL
-		for MUTANT_FILE in $(find mutants -type f | sort); do
-			git apply $MUTANT_FILE
-			MUTANT=$(echo $MUTANT_FILE | grep -o '[0-9][0-9]')
-
+for PROTOCOL in $(ls protocols); do
+	cd protocols/$PROTOCOL
+	for MUTANT_FILE in $(find mutants -type f | sort); do
+		git apply $MUTANT_FILE
+		MUTANT=$(echo $MUTANT_FILE | grep -o '[0-9][0-9]')
+		for SEED in $(cat seeds.txt); do
 			forge clean
 			START=$(date +%s)
-			forge test
+			forge test --fuzz-seed $SEED
 			RESULT=$?
 			END=$(date +%s)
 			TIME=$(echo "$END - $START" | bc)
 
-			echo "foundry,$PROTOCOL,$SEED,$MUTANT,$TIME,$RESULT" >> $RESULTS
+			echo "foundry,$PROTOCOL,$MUTANT,$SEED,$TIME,$RESULT" >> $RESULTS
 
 			forge clean
 			START=$(date +%s)
-			echidna . --contract EchidnaTester --config test/config.yaml >/dev/null
+			echidna . --contract EchidnaTester --config test/config.yaml --seed $SEED >/dev/null
 			RESULT=$?
 			END=$(date +%s)
 			TIME=$(echo "$END - $START" | bc)
 
-			echo "echidna,$PROTOCOL,$SEED,$MUTANT,$TIME,$RESULT" >> $RESULTS
-
-			# cleanup
-			git checkout .
+			echo "echidna,$PROTOCOL,$MUTANT,$SEED,$TIME,$RESULT" >> $RESULTS
 		done
-		cd -
+		# cleanup
+		git checkout .
 	done
+	cd -
 done
