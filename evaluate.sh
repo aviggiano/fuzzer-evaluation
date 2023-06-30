6602874bc04c019ec702f077d27b21e2cd198ba9
Antonio Guilherme Ferreira Viggiano <agfviggiano@gmail.com>
2023-06-30 01:48:26 -0300

    Change all patches to git patch

--- evaluate.sh
+++ evaluate.sh
@@ -5,17 +5,13 @@ set -ux
 for PROTOCOL in $(ls protocols); do
 	cd protocols/$PROTOCOL
 	for MUTANT in $(find mutants -type f | grep -v manual | sort -r); do
-		# some mutants are git patches, other are plain patch files
 		git apply $MUTANT
-		apply < $MUTANT
 
 		forge test
 		echidna . --contract EchidnaTester --config test/config.yaml
 
 		# cleanup
 		git checkout .
-		find . -name '*.sol.orig' -exec rm {} \;
-		find . -name '*.sol.rej' -exec rm {} \;
 	done
 	cd -
 done
