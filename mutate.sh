1d131d43dc4da2e5dbf7642097b6e51468442cbd
Antonio Guilherme Ferreira Viggiano <agfviggiano@gmail.com>
2023-06-30 00:12:12 -0300

    Create mutants in specific folder

--- mutate.sh
+++ mutate.sh
@@ -4,8 +4,13 @@ set -eux
 
 for PROTOCOL in $(ls protocols); do
 	cd protocols/$PROTOCOL
-	for FILE in $(find src -type f -name '*.sol'); do 
-		slither-mutate $FILE --solc-remaps @openzeppelin/=lib/openzeppelin-contracts/contracts --solc-remaps @uniswap/=src/ > $FILE.patch
+	mkdir -p mutants
+
+	mkdir -p mutants/slither
+	i=0
+	for FILE in $(find src -type f -name '*.sol' | grep -v interface); do 
+		let i++
+		slither-mutate $FILE --solc-remaps @openzeppelin/=lib/openzeppelin-contracts/contracts --solc-remaps @uniswap/=src/ > mutants/slither/$(printf "%02d" $i).patch
 	done
 	cd -
 done
