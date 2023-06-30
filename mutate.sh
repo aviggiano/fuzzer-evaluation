d18443dc34d7043a413a4458b08d540a3d18bdd7
Antonio Guilherme Ferreira Viggiano <agfviggiano@gmail.com>
2023-06-30 00:28:26 -0300

    Improve mutation script

--- mutate.sh
+++ mutate.sh
@@ -10,7 +10,13 @@ for PROTOCOL in $(ls protocols); do
 	i=0
 	for FILE in $(find src -type f -name '*.sol'); do 
 		i=$((i+1))
-		slither-mutate $FILE --solc-remaps @openzeppelin/=lib/openzeppelin-contracts/contracts --solc-remaps @uniswap/=src/ > mutants/slither/$(printf "%02d" $i).patch
+		MUTANT=mutants/slither/$(printf "%02d" $i).patch
+		slither-mutate $FILE --solc-remaps @openzeppelin/=lib/openzeppelin-contracts/contracts --solc-remaps @uniswap/=src/ > $MUTANT
+		# ignore files without changes
+		if [ $(wc -l < $MUTANT) -eq 1 ]; then
+			rm $MUTANT
+			i=$((i-1))
+		fi;
 	done
 	cd -
 done
