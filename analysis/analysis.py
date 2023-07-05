c8eb5fc6d6cb578d382d16c5f2e162de00166142
Antonio Guilherme Ferreira Viggiano <agfviggiano@gmail.com>
2023-07-05 16:20:56 -0300

    Use boxplot instead of barchart

--- analysis/analysis.py
+++ analysis/analysis.py
@@ -1,5 +1,4 @@
 import matplotlib.pyplot as plt
-import numpy as np
 import pandas as pd
 import seaborn as sns
 
@@ -9,34 +8,22 @@ df = pd.read_csv("/tmp/final.csv")
 # Convert 'mutant' to integers
 df['mutant'] = df['mutant'].astype(int)
 
+# Convert 'mutant' to two-digit, zero-padded strings
+df['mutant'] = df['mutant'].apply(lambda x: f'{x:02}')
+
 # Sort the DataFrame by 'mutant'
 df = df.sort_values('mutant')
 
-# Calculate the median time for each fuzzer for each mutant
-medians = df.groupby(['mutant', 'fuzzer'])['time'].median().unstack()
-
-# Calculate the standard deviation
-std_devs = df.groupby(['mutant', 'fuzzer'])['time'].std().unstack()
-
-# Create a bar plot
+# Create a boxplot
 plt.figure(figsize=(15,8))
-bar_width = 0.35
-opacity = 0.8
 
-for i, (colname, values) in enumerate(medians.items()):
-    plt.bar(np.arange(len(medians.index)) + (i-0.5)*bar_width, values, width=bar_width, yerr=std_devs[colname],
-            alpha=opacity, color=sns.color_palette('viridis')[i], label=colname)
+# Using seaborn to create boxplot as it supports creating multiple boxes for each category (fuzzers for each mutant) directly
+sns.boxplot(data=df, x='mutant', y='time', hue='fuzzer')
 
 # Set labels and title
 plt.xlabel('Mutant')
-plt.ylabel('Median Time (Log Scale)')
-plt.title('Median Time of Different Fuzzers per Mutant')
-
-# Format mutant values as two-digit, zero-padded strings for x-ticks
-formatted_mutants = [f'{x:02}' for x in medians.index]
-
-# Set x-ticks
-plt.xticks(np.arange(len(medians.index)), formatted_mutants, rotation=90) 
+plt.ylabel('Time (Log Scale)')
+plt.title('Time Distribution of Different Fuzzers per Mutant')
 
 # Set y-axis to logarithmic scale
 plt.yscale("log")
