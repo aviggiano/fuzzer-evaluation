05087274f66c8c8461d23e11e66f725b28d85c25
Antonio Guilherme Ferreira Viggiano <agfviggiano@gmail.com>
2023-07-05 16:04:34 -0300

    Add median and stdev

--- analysis/analysis.py
+++ analysis/analysis.py
@@ -1,15 +1,38 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
+import seaborn as sns
 
-df = pd.read_csv ('/tmp/final.csv')
+# Read data from CSV file
+df = pd.read_csv("/tmp/final.csv")
 
-# x = np.array(df['seed'])
-# y = np.array(df['time'].median())
+# Calculate the median time for each fuzzer for each mutant
+medians = df.groupby(['mutant', 'fuzzer'])['time'].median().unstack()
 
-# plt.bar(x, y, linestyle='None')
+# Calculate the standard deviation
+std_devs = df.groupby(['mutant', 'fuzzer'])['time'].std().unstack()
 
-df.median().plot(kind='bar')
+# Create a bar plot
+plt.figure(figsize=(15,8))
+bar_width = 0.35
+opacity = 0.8
 
-# plt.savefig('figure.png', dpi=400, transparent=True)
-plt.show()
+for i, (colname, values) in enumerate(medians.items()):
+    plt.bar(medians.index + (i-0.5)*bar_width, values, width=bar_width, yerr=std_devs[colname],
+            alpha=opacity, color=sns.color_palette('viridis')[i], label=colname)
+
+# Set labels and title
+plt.xlabel('Mutant')
+plt.ylabel('Median Time (Log Scale)')
+plt.title('Median Time of Different Fuzzers per Mutant')
+plt.xticks(rotation=90)  # Rotate x-axis labels for better readability
+
+# Set y-axis to logarithmic scale
+plt.yscale("log")
+
+# Show legend
+plt.legend()
+
+# Save the plot to a PNG file
+plt.tight_layout()
+plt.savefig("/tmp/final.png")
