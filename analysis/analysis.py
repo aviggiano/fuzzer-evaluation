d20c44461d766fe314957f8d684457a0f0101f4a
Antonio Guilherme Ferreira Viggiano <agfviggiano@gmail.com>
2023-07-05 16:37:25 -0300

    Add background to boxplot

--- analysis/analysis.py
+++ analysis/analysis.py
@@ -15,21 +15,26 @@ df['mutant'] = df['mutant'].apply(lambda x: f'{x:02}')
 df = df.sort_values('mutant')
 
 # Create a boxplot
-plt.figure(figsize=(15,8))
+fig, ax = plt.subplots(figsize=(15,8))
+
+# Add alternating background colors
+for i in range(len(df['mutant'].unique())):
+    if i % 2 == 0:
+        ax.axvspan(i-0.5, i+0.5, facecolor='lightgrey', zorder=0)
 
 # Using seaborn to create boxplot as it supports creating multiple boxes for each category (fuzzers for each mutant) directly
-sns.boxplot(data=df, x='mutant', y='time', hue='fuzzer')
+sns.boxplot(data=df, x='mutant', y='time', hue='fuzzer', ax=ax, zorder=1)
 
 # Set labels and title
-plt.xlabel('Mutant')
-plt.ylabel('Time (Log Scale)')
-plt.title('Time Distribution of Different Fuzzers per Mutant')
+ax.set_xlabel('Mutant')
+ax.set_ylabel('Time (Log Scale)')
+ax.set_title('Time Distribution of Different Fuzzers per Mutant')
 
 # Set y-axis to logarithmic scale
-plt.yscale("log")
+ax.set_yscale("log")
 
 # Show legend
-plt.legend()
+ax.legend()
 
 # Save the plot to a PNG file
 plt.tight_layout()
