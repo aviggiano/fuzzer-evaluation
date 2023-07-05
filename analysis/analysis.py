54ae85b7e75008b53f1af1a439f66487fc14e287
Antonio Guilherme Ferreira Viggiano <agfviggiano@gmail.com>
2023-07-05 16:13:13 -0300

    Plot median and stdev

--- analysis/analysis.py
+++ analysis/analysis.py
@@ -6,6 +6,12 @@ import seaborn as sns
 # Read data from CSV file
 df = pd.read_csv("/tmp/final.csv")
 
+# Convert 'mutant' to integers
+df['mutant'] = df['mutant'].astype(int)
+
+# Sort the DataFrame by 'mutant'
+df = df.sort_values('mutant')
+
 # Calculate the median time for each fuzzer for each mutant
 medians = df.groupby(['mutant', 'fuzzer'])['time'].median().unstack()
 
@@ -18,14 +24,19 @@ bar_width = 0.35
 opacity = 0.8
 
 for i, (colname, values) in enumerate(medians.items()):
-    plt.bar(medians.index + (i-0.5)*bar_width, values, width=bar_width, yerr=std_devs[colname],
+    plt.bar(np.arange(len(medians.index)) + (i-0.5)*bar_width, values, width=bar_width, yerr=std_devs[colname],
             alpha=opacity, color=sns.color_palette('viridis')[i], label=colname)
 
 # Set labels and title
 plt.xlabel('Mutant')
 plt.ylabel('Median Time (Log Scale)')
 plt.title('Median Time of Different Fuzzers per Mutant')
-plt.xticks(rotation=90)  # Rotate x-axis labels for better readability
+
+# Format mutant values as two-digit, zero-padded strings for x-ticks
+formatted_mutants = [f'{x:02}' for x in medians.index]
+
+# Set x-ticks
+plt.xticks(np.arange(len(medians.index)), formatted_mutants, rotation=90) 
 
 # Set y-axis to logarithmic scale
 plt.yscale("log")
