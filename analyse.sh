f118b02175daef9047a863082ad77f7060ab207b
Antonio Guilherme Ferreira Viggiano <agfviggiano@gmail.com>
2023-07-06 19:11:59 -0300

    Fix script to account for subdirectories

--- analyse.sh
+++ analyse.sh
@@ -6,7 +6,7 @@ RESULTS=$(mktemp)
 FINAL=/tmp/final.csv
 
 for FILE in $(aws s3 ls --recursive s3://$S3_BUCKET/ | grep 'results.txt' | awk '{print $NF}'); do
-	INSTANCE_ID=$(echo $FILE | awk -F'/' '{print $1}')
+	INSTANCE_ID=$(echo $FILE | awk -F'/' '{print $(NF-1)}')
 	TMP=$(mktemp)
 	aws s3 cp s3://$S3_BUCKET/$FILE $TMP
 	head -1 $TMP | sed "s/$/,instance_id/" > $FINAL
