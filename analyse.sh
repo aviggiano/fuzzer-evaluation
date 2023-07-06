3b8bffeaef1cfaecd42fe5bad1dfbc04ee3f1059
Antonio Guilherme Ferreira Viggiano <agfviggiano@gmail.com>
2023-07-06 19:18:51 -0300

    Remove old files

--- analyse.sh
+++ analyse.sh
@@ -5,7 +5,7 @@ S3_BUCKET="$1"
 RESULTS=$(mktemp)
 FINAL=/tmp/final.csv
 
-for FILE in $(aws s3 ls --recursive s3://$S3_BUCKET/ | grep 'results.txt' | awk '{print $NF}'); do
+for FILE in $(aws s3 ls --recursive s3://$S3_BUCKET/ | grep -v 'old' | grep 'results.txt' | awk '{print $NF}'); do
 	INSTANCE_ID=$(echo $FILE | awk -F'/' '{print $(NF-1)}')
 	TMP=$(mktemp)
 	aws s3 cp s3://$S3_BUCKET/$FILE $TMP
