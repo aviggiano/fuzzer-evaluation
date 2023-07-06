9d3c14ef54330c2165540a4244c78e645ffda830
Antonio Guilherme Ferreira Viggiano <agfviggiano@gmail.com>
2023-07-06 17:25:33 -0300

    Make scripts more generic

--- Makefile
+++ Makefile
@@ -1,12 +1,15 @@
 default: help
 
+mutant ?= .
+fuzzer ?= .
+
 help:
 	@echo "usage:"
-	@echo "	make test			fuzz against default codebase"
-	@echo "	make mutate			create patch files using mutation tools"
-	@echo "	make evaluate <seed>		evaluate fuzzers after applying mutations for a particular seed"
-	@echo "	make terraform			use terraform to deploy the infrastructure and start the evaluation"
-	@echo "	make analyse <s3_bucket>	download results files from S3 for all launched instances and analyse the results"
+	@echo "	make test					fuzz against default codebase"
+	@echo "	make mutate					create patch files using mutation tools"
+	@echo "	make evaluate <seed> [<mutant> <fuzzer>]	evaluate fuzzers for a particular seed and optionally a mutant and a fuzzer"
+	@echo "	make terraform					use terraform to deploy the infrastructure and start the evaluation"
+	@echo "	make analyse <s3_bucket>			download results files from S3 for all launched instances and analyse the results"
 
 test:
 	./test.sh
@@ -15,7 +18,7 @@ mutate:
 	./mutate.sh
 
 evaluate:
-	./evaluate.sh $(seed)
+	./evaluate.sh $(seed) $(mutant) $(fuzzer)
 
 terraform:
 	( cd terraform && terraform apply );
--- evaluate.sh
+++ evaluate.sh
@@ -3,6 +3,8 @@
 set -ux
 
 SEED="$1"
+MUTANT="$2"
+FUZZER="$3"
 
 TIMEOUT=86400
 RESULTS="$(pwd)/results.txt"
@@ -22,27 +24,29 @@ echo "fuzzer,protocol,seed,mutant,time,result" > $RESULTS
 
 for PROTOCOL in $(ls protocols); do
 	cd protocols/$PROTOCOL
-	for MUTANT_FILE in $(find mutants -type f | sort -r -t'/' -k 3,3); do
+	for MUTANT_FILE in $(find mutants -type f | sort -r -t'/' -k 3,3 | grep "$MUTANT"); do
 		git apply $MUTANT_FILE
 		MUTANT=$(echo $MUTANT_FILE | grep -o '[0-9][0-9]')
 
-		forge clean
-		START=$(date +%s)
-		timeout -k 10 $TIMEOUT forge test --fuzz-seed $SEED
-		RESULT=$?
-		END=$(date +%s)
-		TIME=$(echo "$END - $START" | bc)
+		if [ $(echo "forge" | grep "$FUZZER" | wc -l) -gt 0 ]; then
+			forge clean
+			START=$(date +%s)
+			timeout -k 10 $TIMEOUT forge test --fuzz-seed $SEED
+			RESULT=$?
+			END=$(date +%s)
+			TIME=$(echo "$END - $START" | bc)
+			echo "foundry,$PROTOCOL,$SEED,$MUTANT,$TIME,$RESULT" >> $RESULTS
+		fi
 
-		echo "foundry,$PROTOCOL,$SEED,$MUTANT,$TIME,$RESULT" >> $RESULTS
-
-		forge clean
-		START=$(date +%s)
-		timeout -k 10 $TIMEOUT echidna . --contract EchidnaTester --config test/config.yaml --workers $WORKERS --seed $SEED >/dev/null
-		RESULT=$?
-		END=$(date +%s)
-		TIME=$(echo "$END - $START" | bc)
-
-		echo "echidna,$PROTOCOL,$SEED,$MUTANT,$TIME,$RESULT" >> $RESULTS
+		if [ $(echo "echidna" | grep "$FUZZER" | wc -l) -gt 0 ]; then
+			forge clean
+			START=$(date +%s)
+			timeout -k 10 $TIMEOUT echidna . --contract EchidnaTester --config test/config.yaml --workers $WORKERS --seed $SEED >/dev/null
+			RESULT=$?
+			END=$(date +%s)
+			TIME=$(echo "$END - $START" | bc)
+			echo "echidna,$PROTOCOL,$SEED,$MUTANT,$TIME,$RESULT" >> $RESULTS
+		fi
 
 		# cleanup
 		git checkout .
